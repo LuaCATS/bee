cac602d77d19f202cabd741a572a93078b8b20e8
fesily <fesil@foxmail.com>
2024-10-19 15:07:19 +0800

    update meta

---
+++ library/bee/time.lua
@@ -0,0 +1,9 @@
+---@meta
+local m = {}
+---@return integer nsec
+function m.time() end
+---@return integer nsec
+function m.monotonic() end
+---@return integer nsec
+function m.thread() end
+return m
